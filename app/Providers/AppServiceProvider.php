f060b3c8d9fc83fc7d7f36c8ead87a5ffb875d41
namdosatdn@gmail.com <namdosatdn@gmail.com>
2018-04-23 23:36:07 +0700

    s

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \Schema::defaultStringLength(191);
-        if(Schema::hasTable('category')){
+        if(\Schema::hasTable('category')){
               $categories=Categories::pluck('name','id')->all();
               $categories['default']='--Chọn lọai sản phẩm--'  ;
               ksort($categories);
